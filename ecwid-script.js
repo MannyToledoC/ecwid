fd1cd2c16c338db461f72c6e9aa5e97555e73f1c
MannyToledoC <mannytoledoc@gmail.com>
2025-01-27 18:20:08 -0500

    Add files via upload

---
+++ ecwid-script.js
@@ -0,0 +1,13 @@
+Ecwid.OnAPILoaded.add(function() {
+	Ecwid.OnPageLoaded.add(function(page) {
+    if (page.type == "PRODUCT") {
+       console.log(
+        `
+       Page loaded!
+       Ecwid store ID is: ${Ecwid.getOwnerId()}
+       Product ID is: ${page.productId}
+        `
+       )
+    }
+	})
+})
