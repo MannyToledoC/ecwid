5ceabd91fad1c4cc5c594e2a6728a0e5f30b1d2b
MannyToledoC <mannytoledoc@gmail.com>
2025-01-29 14:14:08 -0500

    Add files via upload

--- ecwid-script.js
+++ ecwid-script.js
@@ -1,16 +1,13 @@
-Ecwid.OnAPILoaded.add(function() {
-	Ecwid.OnPageLoaded.add(function(page) {
-    if (page.type == "PRODUCT") {
-      console.log(
-        `
-        Page loaded!
-        Ecwid store ID is: ${Ecwid.getOwnerId()}
-        Product ID is: ${page.productId}
-        `
-      )
-      Ecwid.Product.get((product) => {
-        console.log(product.variations)
-      })
-    }
-	})
-})
+// Get ecwid app api thing
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://djqizrxa6f10j.cloudfront.net/ecwid-sdk/js/1.3.0/ecwid-app.js";
+document.head.appendChild(s);
+
+EcwidApp.init({
+  app_id: "custom-app-100229268-1", // Replace with your actual app ID
+  autoloadedflag: true,
+  autoheight: true
+});
+
+let storeData = EcwidApp.getPayLoad();
